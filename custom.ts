53e59464a2ff09039f6d948c94090b146e969887
okanagan-code-academy <157773119+okanagan-code-academy@users.noreply.github.com>
2025-02-20 23:32:42 -0800

    Update main.blocks, main.ts, custom.ts, images.g.jres, images.g.ts

--- custom.ts
+++ custom.ts
@@ -56,3 +56,14 @@ namespace Math {
         return value0 + t * (value1 - value0);
     }
 }
+//% weight=100 color=#8E2EC4 icon=""
+namespace game {
+    /**
+    * The amount of time elapsed per frame
+    *
+    */
+    //% block
+    export function getDeltaTime(): number {
+        return control.eventContext().deltaTimeMillis
+    }
+}
--- main.ts
+++ main.ts
@@ -1,7 +1,8 @@
 namespace SpriteKind {
     export const Cursor = SpriteKind.create()
 }
-let distanceToCursor: spriteutils.Position = null
+let projectileSprite: Sprite = null
+let distanceToCursor = null
 let playerShip = sprites.create(assets.image`ship`, SpriteKind.Player)
 controller.moveSprite(playerShip)
 let cursorSprite = sprites.create(img`
@@ -22,8 +23,23 @@ let cursorSprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Cursor)
+game.onUpdate(function () {
+	
+})
 forever(function () {
-    distanceToCursor = spriteutils.pos(0, 0)
     cursorSprite.setPosition(Math.map(controller.acceleration(ControllerDimension.X), 0, 2048, 0, 320) + 160, Math.map(controller.acceleration(ControllerDimension.Y), 0, 2048, 0, 240) + 120)
-    transformSprites.rotateSprite(playerShip, spriteutils.radiansToDegrees(Math.atan2(cursorSprite.y - playerShip.y, cursorSprite.x - playerShip.x)))
+    transformSprites.rotateSprite(playerShip, Math.lerp(transformSprites.getRotation(playerShip), spriteutils.radiansToDegrees(Math.atan2(cursorSprite.y - playerShip.y, cursorSprite.x - playerShip.x)), 1 - 2 ** (-0.005 * game.getDeltaTime())))
+})
+forever(function () {
+    if (controller.A.isPressed()) {
+        projectileSprite = sprites.create(assets.image`projectile`, SpriteKind.Projectile)
+        spriteutils.placeAngleFrom(
+        projectileSprite,
+        spriteutils.degreesToRadians(transformSprites.getRotation(playerShip)),
+        10,
+        playerShip
+        )
+        spriteutils.setVelocityAtAngle(projectileSprite, spriteutils.degreesToRadians(transformSprites.getRotation(playerShip)), 200)
+        pause(100)
+    }
 })
